f95d20d1af4e8fbeb86dda5ce3a2ca529c05ad9b
dcntrl <dcntrlx@gmail.com>
2025-12-09 19:01:21 +0300

    Add deploy script for Campaign.sol contract

---
+++ contracts/script/Campaign.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {Campaign} from "../src/Campaign.sol";
+
+contract CampaignScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        Campaign campaign = new Campaign("Campaign", 100, 100);
+        vm.stopBroadcast();
+    }
+}
