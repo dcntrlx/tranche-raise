582b88cdd17cc542e8cffb444d88aa52d8e549eb
dcntrl <dcntrlx@gmail.com>
2025-12-11 22:23:07 +0300

    Provide complete CampaignDetails frontend. Optimize CampaignFactory

--- client/src/app/campaigns/[address]/page.tsx
+++ client/src/app/campaigns/[address]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { use } from "react";
-import { useReadContract } from "wagmi";
+import { useReadContract, useReadContracts } from "wagmi";
 import { notFound } from "next/navigation";
 
 import { CAMPAIGN_ABI } from "../../contracts";
@@ -9,13 +9,32 @@ import { CAMPAIGN_ABI } from "../../contracts";
 export default function CampaignDetails({ params }: { params: Promise<{ address: string }> }) {
     const address = (use(params)).address as `0x${string}`;
 
-    const { data: campaignTitle, isLoading: isLoadingCampaignTitle } = useReadContract({
-        address,
-        abi: CAMPAIGN_ABI,
-        functionName: 'campaignTitle'
+    const { data: campaignData, isLoading: isLoadingCampaignData } = useReadContracts({
+        contracts: [
+            { address, abi: CAMPAIGN_ABI, functionName: 'campaignTitle' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'CAMPAIGN_GOAL' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'CAMPAIGN_DURATION' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'campaignStart' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'campaignEnd' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'OWNER' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'totalDistributed' },
+            { address, abi: CAMPAIGN_ABI, functionName: 'totalRaised' },
+        ]
     })
 
-    if (isLoadingCampaignTitle) {
+    const [
+        campaignTitle,
+        campaignGoal,
+        campaignDuration,
+        campaignStart,
+        campaignEnd,
+        owner,
+        totalDistributed,
+        totalRaised
+    ] = campaignData?.map(data => data.result) ?? []
+
+    console.log(campaignData)
+    if (isLoadingCampaignData) {
         return <div>Loading campaign details...</div>
     }
 
@@ -26,8 +45,14 @@ export default function CampaignDetails({ params }: { params: Promise<{ address:
     return (
         <div>
             <h1>Campaign Details:</h1>
-            <p>{campaignTitle}</p>
-            <p>{address}</p>
+            <p>campaignTitle: {campaignTitle}</p>
+            <p>campaignGoal: {campaignGoal}</p>
+            <p>campaignDuration: {campaignDuration}</p>
+            <p>campaignStart: {campaignStart}</p>
+            <p>campaignEnd: {campaignEnd}</p>
+            <p>owner: {owner}</p>
+            <p>totalDistributed: {totalDistributed}</p>
+            <p>totalRaised: {totalRaised}</p>
         </div>
     )
 }
--- client/src/app/campaigns/page.tsx
+++ client/src/app/campaigns/page.tsx
@@ -12,6 +12,8 @@ export default function Campaigns() {
         functionName: 'getAllCampaigns'
     })
 
+
+
     console.log(campaigns)
     return (
         <div>
